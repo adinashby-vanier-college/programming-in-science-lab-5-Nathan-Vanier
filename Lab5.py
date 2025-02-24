481f9d8971b90bc7a10e0e1347f39ebea9c46fa1
Nathan-Vanier <nathan@alertepi.com>
2025-02-24 09:42:26 -0500

    changed the range

--- Lab5.py
+++ Lab5.py
@@ -12,12 +12,12 @@ def hollow_square(n):
 # 123
 # 1234
 def number_pattern(n):
- for i in range(1, 4):
+ for i in range(1, n):
   result = ''
   for j in range(1, i + 1):
     result = str(j)
       # this adds it to a string
-    return(n, "")
+    return(result, "")
     
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
