75aa7ab1e289571278acc82fa22b1a15093f5c51
Nathan-Vanier <nathan@alertepi.com>
2025-02-24 09:37:05 -0500

    .

--- Lab5.py
+++ Lab5.py
@@ -17,7 +17,7 @@ def number_pattern(n):
   for j in range(1, i + 1):
     result = str(j)
       # this adds it to a string
-    return(result, "")
+    return(n, "")
     
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
